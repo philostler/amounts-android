c72a7a05e25a81d3ae63cce8ecadb6e1535ac271
Phil Ostler <philostler@gmail.com>
2013-10-18 17:47:20 +0100

    Improves padding and fonts on converted ListView

--- Amounts/src/main/java/io/bass64/amounts/ConversationAdapter.java
+++ Amounts/src/main/java/io/bass64/amounts/ConversationAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import java.util.List;
 
 import io.bass64.amounts.models.ConversationResult;
+import io.bass64.amounts.utils.FontUtils;
 
 /**
  * Created by philostler on 17/10/13.
@@ -22,9 +23,13 @@ public class ConversationAdapter<T> extends ArrayAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = super.getView(position, convertView, parent);
 
+        TextView unitOfMeasure = (TextView)view.findViewById(R.id.unitOfMeasure);
+        FontUtils.setFont(getContext(), unitOfMeasure);
+
         ConversationResult result = (ConversationResult)getItem(position);
         TextView convertedValue = (TextView)view.findViewById(R.id.convertedValue);
         convertedValue.setText(String.valueOf(result.value));
+        FontUtils.setFont(getContext(), convertedValue);
 
         return view;
     }
